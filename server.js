e40632a1bee4c98ed3d2d4ede9b4343391a24e14
Alan Astudillo <alanastudillo@hotmail.com>
2018-03-30 14:48:57 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+//Declarar objeto
+var app = express();
+
+// Registrar carpeta donde se almacena 'Base' html
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Definir el tipo de templating que se utilizara
+app.set('view engine', 'hbs');
+
+//Middleware
+
+//Loggear
+app.use( (req, res, next) => {
+  var now = new Date().toString();
+
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+
+  //Agregar a archivo
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if(error) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  //Continuar
+  next();
+});
+
+//Modo de mantenimiento
+// app.use( (req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+//Definir directorio de assets estaticos
+app.use(express.static(__dirname + '/public'));
+
+//Helpers para templating
+
+//Funcion que retorna el año actual
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+//Funcion que convierte un string a mayusculas
+hbs.registerHelper('toUpperCase', (text) => {
+  return text.toUpperCase();
+});
+
+//Routing
+
+//Root
+app.get('/', (req, res) => {
+
+  //Mostrar HTML y pasar variables
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my site!'
+  });
+
+});
+
+//About
+app.get('/about', (req, res) => {
+
+  //Mostrar HTML y pasar variables
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+
+});
+
+//Bad
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to fullfill request.'
+  });
+});
+
+//Inicializar
+app.listen(3000);
